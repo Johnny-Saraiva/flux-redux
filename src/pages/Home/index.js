b3c32a7b94ec9c950d83f1267c1a044bc1b09092
Johnny Saraiva <johnny.saraiva.dev@gmail.com>
2020-11-28 21:13:22 -0300

    Configure home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,108 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-prophecy-9/46/D16-5355-446/D16-5355-446_zoom1.jpg?ts=1599149600&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Mizuno</strong>
+        <span>R$799,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-prophecy-9/46/D16-5355-446/D16-5355-446_zoom1.jpg?ts=1599149600&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Mizuno</strong>
+        <span>R$799,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-prophecy-9/46/D16-5355-446/D16-5355-446_zoom1.jpg?ts=1599149600&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Mizuno</strong>
+        <span>R$799,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-prophecy-9/46/D16-5355-446/D16-5355-446_zoom1.jpg?ts=1599149600&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Mizuno</strong>
+        <span>R$799,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-prophecy-9/46/D16-5355-446/D16-5355-446_zoom1.jpg?ts=1599149600&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Mizuno</strong>
+        <span>R$799,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-prophecy-9/46/D16-5355-446/D16-5355-446_zoom1.jpg?ts=1599149600&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis Mizuno</strong>
+        <span>R$799,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
 
