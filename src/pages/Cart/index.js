7dcd2e0bfc82bda74d9c2e90dd4808fad1d5e3ac
Johnny Saraiva <johnny.saraiva.dev@gmail.com>
2020-11-28 22:18:14 -0300

    Configure cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,12 +1,69 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 function Cart() {
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th> </th>
+            <th>Produto</th>
+            <th>Qtd</th>
+            <th>Subtotal</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-mizuno-wave-prophecy-9/46/D16-5355-446/D16-5355-446_zoom1.jpg?ts=1599149600&ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis Mizuno</strong>
+              <span>R$799,99</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$1599,98</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
 
