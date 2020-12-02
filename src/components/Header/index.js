7257c40a0579060929e16cd0ee04eaaf1b342d03
Johnny Saraiva <johnny.saraiva.dev@gmail.com>
2020-12-01 22:02:21 -0300

    Starting to configure redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { MdShoppingCart } from 'react-icons/md';
@@ -8,6 +9,10 @@ import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
+  const { cart } = useSelector((state) => ({
+    cart: state.cart.length,
+  }));
+
   return (
     <Container>
       <Link to="/">
@@ -17,7 +22,7 @@ export default function Header() {
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>3 itens</span>
+          <span>{cart} itens</span>
         </div>
         <MdShoppingCart size={36} color="#FFF" />
       </Cart>
